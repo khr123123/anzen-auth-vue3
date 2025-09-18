0e81aae2624860f80aac11addb6e3ee9935b65aa
3031208 <21@qwe>
2025-09-18 10:25:26 +0900

    feat: 集成 Arco Design 组件库
    - 在项目中添加 @arco-design/web-vue 依赖
    - 在 main.ts 中引入并注册 ArcoVue
    - 修改 App.vue 中的模板，使用 Arco Design 组件
    - 更新 tsconfig.json，配置模块别名
    - 修改 vite.config.ts，添加别名和扩展名配置

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <a-space>
+      <a-button type="primary">Primary</a-button>
+      <a-button>Secondary</a-button>
+      <a-button type="dashed">Dashed</a-button>
+      <a-button type="outline">Outline</a-button>
+      <a-button type="text">Text</a-button>
+    </a-space>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -21,9 +22,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
